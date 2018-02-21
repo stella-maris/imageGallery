8df46ed5aea4fd8f4bd50cb51e0712bba8295ef9
stella-maris <mango.gal@gmail.com>
2018-02-20 23:03:37 -0800

    adds show and hide on scroll

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 (function() {
+    var navbar = document.getElementsByTagName("nav")[0];
+    navbar.style.display = "none";
+    
     window.onscroll = function() {
     console.log('scrolling');
-        var navbar = document.getElementsByTagName("nav")[0];
+        navbar.style.display = "block";
         setTimeout(function () {
             navbar.style.position = "fixed";
+            navbar.style.top = "0px";
             //slide into view
             //position fixed
         }, 500);
