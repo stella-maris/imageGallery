4f646f54a3b70a5ac259f276fe9f63cad16557cb
stella-maris <mango.gal@gmail.com>
2018-02-20 22:56:27 -0800

    scroll event

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 (function() {
     window.onscroll = function() {
     console.log('scrolling');
-        var navbar = document.getElementsByTagName("nav");
+        var navbar = document.getElementsByTagName("nav")[0];
         setTimeout(function () {
+            navbar.style.position = "fixed";
             //slide into view
             //position fixed
         }, 500);
