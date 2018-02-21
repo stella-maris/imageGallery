5dd5fd98f1cd3716afc94b98284b402e110eb09c
stella-maris <mango.gal@gmail.com>
2018-02-20 22:52:33 -0800

    adding event listener

---
+++ index.js
@@ -0,0 +1,10 @@
+(function() {
+    window.onscroll = function() {
+    console.log('scrolling');
+        var navbar = document.getElementsByTagName("nav");
+        setTimeout(function () {
+            //slide into view
+            //position fixed
+        }, 500);
+    };
+})()
