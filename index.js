9244420d848404fe5e33feaee0e26252533f30e5
stella-maris <mango.gal@gmail.com>
2018-02-21 21:34:37 -0800

    adds css animation

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 (function() {
     var navbar = document.getElementsByTagName("nav")[0];
-    navbar.style.display = "none";
+    navbar.classList.toggle('fadeIn');
     
     window.onscroll = function() {
-    console.log('scrolling');
-        navbar.style.display = "block";
-        setTimeout(function () {
-            navbar.style.position = "fixed";
-            navbar.style.top = "0px";
-            //slide into view
-            //hide navbar when page is at top
-        }, 500);
+        console.log('scrolling');
     };
     
     var wrapper = document.getElementsByClassName("wrapper");
