63d67bd80ab28e618759dfaa6230187c90dfd367
stella-maris <mango.gal@gmail.com>
2018-02-21 20:15:47 -0800

    adds animation

--- index.js
+++ index.js
@@ -9,7 +9,22 @@
             navbar.style.position = "fixed";
             navbar.style.top = "0px";
             //slide into view
-            //position fixed
+            //hide navbar when page is at top
         }, 500);
     };
+    
+    var wrapper = document.getElementsByClassName("wrapper");
+    
+    function expandImage() {
+        if (this.classList.contains("open")) {
+            this.style.zIndex = 0;
+        } else {
+            this.style.zIndex = 10;
+        }
+        this.classList.toggle("open");    
+    };
+
+    for (i = 0; i < wrapper.length; i++) {
+        wrapper[i].addEventListener("click", expandImage);
+    } 
 })()
